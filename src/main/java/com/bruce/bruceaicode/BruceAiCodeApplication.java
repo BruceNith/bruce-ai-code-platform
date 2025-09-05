e841363e7cef06f0160aa044887d75a72d6cc3a7
BruceNith <nitianhua@outlook.com>
2025-09-05 22:09:22 +0800

    initial commit

---
+++ src/main/java/com/bruce/bruceaicode/BruceAiCodeApplication.java
@@ -0,0 +1,15 @@
+package com.bruce.bruceaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)     // 开启AOP 通过AOP对当前代理对象的访问，使得在业务中访问到当前的代理对象
+public class BruceAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruceAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bruce/bruceaicode/BruceAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.bruceaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruceAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
